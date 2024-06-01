f62a4bf310e27c0a9bfc555b563928da467066a3
Saad1926Q <142170496+Saad1926Q@users.noreply.github.com>
2024-06-01 11:34:28 +0530

    first commit

---
+++ battles.js
@@ -0,0 +1,123 @@
+const searchBtn=document.getElementById("search-btn")
+
+let rightScore,leftScore;
+
+async function fetchData(searchBar){
+    try{
+        var myPokemon = searchBar.value;
+
+        var response = await fetch(`https://pokeapi.co/api/v2/pokemon/${myPokemon}`);
+        var data = await response.json();
+
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayPokemondata(card,pokemonData,playerNumber){
+    card.innerHTML=`
+    <img src="${pokemonData.sprites.front_default}">
+    <h2>${pokemonData.name}</h2>
+    <p><strong>Attack:</strong><span>${pokemonData.stats[1].base_stat}</span></p>
+    <p><strong>Defense:</strong><span>${pokemonData.stats[2].base_stat}</span></p>
+    <p><strong>Hp:</strong><span>${pokemonData.stats[0].base_stat}</span></p>
+    <p><strong>Height:</strong><span>${pokemonData.height}</span></p>
+    <p><strong>Weight:</strong><span>${pokemonData.weight}</span></p>`;
+
+    if(playerNumber===0){
+        document.getElementById("left-heading").innerHTML=`Player 1 chose ${pokemonData.name}`
+        document.getElementById("left-card").style.display="block";
+    }
+    else{
+        document.getElementById("right-heading").innerHTML=`Player 2 chose ${pokemonData.name}`
+        document.getElementById("right-card").style.display="block";
+    }
+}
+
+function displayResult(leftScore,rightScore){
+    if(leftScore>rightScore){
+        document.getElementById("result-display").innerHTML="Player 1 Wins"
+    }
+    else if(rightScore>leftScore){
+        document.getElementById("result-display").innerHTML="Player 2 Wins"
+    }
+    else{
+        document.getElementById("result-display").innerHTML="Its A Draw"
+    }
+}
+
+
+function replayGame(){
+    document.getElementById("left-card").innerHTML="";
+    document.getElementById("left-card").style.display="none";
+    document.getElementById("right-card").innerHTML="";
+    document.getElementById("right-card").style.display="none";
+    document.getElementById("left-heading").innerHTML="";
+    document.getElementById("right-heading").innerHTML="";
+    document.getElementById("left-score").innerHTML="";
+    document.getElementById("right-score").innerHTML="";
+    document.getElementById("result-display").innerHTML="";
+    document.getElementById("play-again-btn").style.display="none";
+
+    document.getElementById("search-bar-1").value="";
+    document.getElementById("search-bar-2").value="";
+
+    leftScore=0;
+    rightScore=0;
+}
+
+async function fetchBothPokemons(){
+    try{
+        const searchBarLeft = document.getElementById("search-bar-1");
+        const searchBarRight = document.getElementById("search-bar-2");
+
+        const [dataLeft, dataRight] = await Promise.all([
+            fetchData(searchBarLeft),
+            fetchData(searchBarRight)
+        ]);
+
+        console.log('Left Pokemon:', dataLeft);
+        console.log('Right Pokemon:', dataRight);
+
+        displayPokemondata(document.getElementById("left-card"),dataLeft,0);
+        displayPokemondata(document.getElementById("right-card"),dataRight,1);
+
+
+        leftScore=dataLeft.stats[1].base_stat+dataLeft.stats[2].base_stat+dataLeft.stats[0].base_stat;
+        rightScore=dataRight.stats[1].base_stat+dataRight.stats[2].base_stat+dataRight.stats[0].base_stat
+
+        document.getElementById("left-score").innerHTML=`<strong>Score:</strong><span>${leftScore}</span>`
+        document.getElementById("right-score").innerHTML=`<strong>Score:</strong><span>${rightScore}</span>`
+
+        displayResult(leftScore,rightScore);
+
+        document.getElementById("play-again-btn").style.display="block";
+
+    }catch(error){
+        console.log(error)
+    }
+}
+
+
+searchBtn.addEventListener("click",()=>{
+    fetchBothPokemons();
+});
+
+document.getElementById("play-again-btn").addEventListener("click",()=>{
+    replayGame();
+})
+
+document.getElementById("home-btn").addEventListener("click",()=>{
+    window.location.href = "/Pokedex Project/index.html";
+})
+
+document.getElementById("about-btn").addEventListener("click",()=>{
+    document.querySelector(".modal").style.display="block";
+})
+
+window.addEventListener("click",(event)=>{
+    if(event.target==document.querySelector(".modal")){
+        document.querySelector(".modal").style.display="none";
+    }
+})
---
+++ main.js
@@ -0,0 +1,149 @@
+const searchBar=document.getElementById("search-bar")
+const searchBtn=document.getElementById("search-btn")
+
+let cardsList=[];
+let cardsHistory;
+var historyIndex;
+var historyCardKey;
+lastUpdated=0;
+
+
+if(localStorage.cardsHistory){
+    cardsHistory=JSON.parse(localStorage.getItem("cardsHistory"));
+}
+else{
+    cardsHistory={};
+}
+
+async function fetchData(){
+    try{
+        var myPokemon=searchBar.value;
+
+        var response=await fetch(`https://pokeapi.co/api/v2/pokemon/${myPokemon}`)
+
+        var data = await response.json();
+
+        return data;
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+searchBtn.addEventListener("click",()=>{
+    fetchData()
+    .then(data=>{
+        console.log(data)
+        
+        if (cardsList.length===0) {
+
+
+            var cardContent = `
+        <img src="${data.sprites.front_default}">
+        <h2>${data.name}</h2>
+        <p><strong>Attack:</strong><span>${data.stats[1].base_stat}</span></p>
+        <p><strong>Defense:</strong><span>${data.stats[2].base_stat}</span></p>
+        <p><strong>Hp:</strong><span>${data.stats[0].base_stat}</span></p>
+        <p><strong>Height:</strong><span>${data.height}</span></p>
+        <p><strong>Weight:</strong><span>${data.weight}</span></p>
+        `
+            cardsList[0]=cardContent;
+            historyIndex=Object.keys(cardsHistory).length+1;
+            historyCardKey=`card-${historyIndex}`
+            cardsHistory[historyCardKey]=cardContent
+            console.log(cardsHistory)
+            document.getElementById("card-1").innerHTML=cardContent;
+            document.getElementById("card-1").style.display="block";
+        }
+        else if (lastUpdated < 2) {
+
+            var cardContent = `
+            <img src="${data.sprites.front_default}">
+            <h2>${data.name}</h2>
+            <p><strong>Attack:</strong><span>${data.stats[1].base_stat}</span></p>
+            <p><strong>Defense:</strong><span>${data.stats[2].base_stat}</span></p>
+            <p><strong>Hp:</strong><span>${data.stats[0].base_stat}</span></p>
+            <p><strong>Height:</strong><span>${data.height}</span></p>
+            <p><strong>Weight:</strong><span>${data.weight}</span></p>
+        `
+            cardsList[lastUpdated+1]=cardContent;
+            historyIndex=Object.keys(cardsHistory).length+1;
+            historyCardKey=`card-${historyIndex}`
+            cardsHistory[historyCardKey]=cardContent
+            console.log(cardsHistory)
+            document.getElementById(`card-${lastUpdated+2}`).innerHTML=cardContent;
+            document.getElementById(`card-${lastUpdated+2}`).style.display="block";
+            lastUpdated++;
+            if((cardsList.length===2)&&(document.getElementById("back-btn").style.display==="none")&&(document.getElementById("next-btn").style.display==="none")){
+                document.getElementById("back-btn").style.display="block";
+                document.getElementById("next-btn").style.display="block";
+            }
+        }
+        else if(lastUpdated==2){
+            var cardContent = `
+            <img src="${data.sprites.front_default}">
+            <h2>${data.name}</h2>
+            <p><strong>Attack:</strong><span>${data.stats[1].base_stat}</span></p>
+            <p><strong>Defense:</strong><span>${data.stats[2].base_stat}</span></p>
+            <p><strong>Hp:</strong><span>${data.stats[0].base_stat}</span></p>
+            <p><strong>Height:</strong><span>${data.height}</span></p>
+            <p><strong>Weight:</strong><span>${data.weight}</span></p>
+            `
+                cardsList[0]=cardContent;
+                historyIndex=Object.keys(cardsHistory).length+1;
+                historyCardKey=`card-${historyIndex}`
+                cardsHistory[historyCardKey]=cardContent
+                console.log(cardsHistory)
+                document.getElementById(`card-1`).innerHTML=cardContent;
+                document.getElementById(`card-1`).style.display="block";
+                lastUpdated=0;
+        }
+        localStorage.setItem("cardsHistory",JSON.stringify(cardsHistory));
+    })
+})
+
+document.getElementById("next-btn").addEventListener("click",()=>{
+    var leftElement=cardsList[0];
+    var middleElement=cardsList[1];
+    var rightElement=cardsList[2];
+
+    cardsList[0]=rightElement;
+    cardsList[1]=leftElement;
+    cardsList[2]=middleElement;
+
+    document.getElementById(`card-1`).innerHTML=cardsList[0];
+    document.getElementById(`card-2`).innerHTML=cardsList[1];
+    document.getElementById(`card-3`).innerHTML=cardsList[2];
+})
+
+document.getElementById("back-btn").addEventListener("click",()=>{
+    var leftElement=cardsList[0];
+    var middleElement=cardsList[1];
+    var rightElement=cardsList[2];
+    
+    cardsList[0]=middleElement;
+    cardsList[1]=rightElement;
+    cardsList[2]=leftElement;
+
+    document.getElementById(`card-1`).innerHTML=cardsList[0];
+    document.getElementById(`card-2`).innerHTML=cardsList[1];
+    document.getElementById(`card-3`).innerHTML=cardsList[2];
+})
+
+document.getElementById("history-btn").addEventListener("click", () => {
+    window.location.href = "/Pokedex Project/history.html";
+});
+
+document.getElementById("battle-btn").addEventListener("click",()=>{
+    window.location.href = "/Pokedex Project/battle.html";
+})
+
+document.getElementById("about-btn").addEventListener("click",()=>{
+    document.querySelector(".modal").style.display="block";
+})
+
+window.addEventListener("click",(event)=>{
+    if(event.target==document.querySelector(".modal")){
+        document.querySelector(".modal").style.display="none";
+    }
+})
